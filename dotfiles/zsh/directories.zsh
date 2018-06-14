dca916db61776cae5c9efb7a5b9c95c99aaf232f
qzed <qzed@users.noreply.github.com>
2018-06-14 02:16:52 +0200

    Use exa instead of ls where available

--- dotfiles/zsh/directories.zsh
+++ dotfiles/zsh/directories.zsh
@@ -10,6 +10,12 @@ alias d="dirs -v | head -10"        # Alias for displaying directory stack
 alias pd="popd"
 
 # Sortcuts for `ls`
-alias ll="ls -lhF --color=auto"
-alias la="ls -lAhF --color=auto"
-alias ls="ls -F --color=auto"
+if command -v "exa" >/dev/null 2>&1 ; then          # Use `exa` if available
+    alias ll="exa -lhb"
+    alias la="exa -lhba"
+    alias ls="exa"
+else                                                # Fallback to `ls`
+    alias ll="ls -lhF --color=auto"
+    alias la="ls -lAhF --color=auto"
+    alias ls="ls -F --color=auto"
+fi
