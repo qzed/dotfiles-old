0f7ab818deca81e3f116eeeeb9eb8391107a4125
qzed <qzed@users.noreply.github.com>
2019-08-07 00:32:19 +0200

    Add source-hilight support for PKGBUILDs

--- dotfiles/source-highlight-lesspipe/src-hilite-lesspipe.sh
+++ dotfiles/source-highlight-lesspipe/src-hilite-lesspipe.sh
@@ -6,6 +6,8 @@ for source in "$@"; do
         source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=esc.style -i "$source" ;;
 	*Makefile|*makefile) 
         source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc.style -i "$source" ;;
+	*PKGBUILD) 
+        source-highlight --failsafe -f esc --lang-def=sh.lang --style-file=esc.style -i "$source" ;;
 	*.zsh)
         source-highlight --failsafe -f esc --lang-def=zsh.lang --style-file=esc.style -i "$source" ;;
 	*.tar|*.tgz|*.gz|*.bz2|*.xz)
